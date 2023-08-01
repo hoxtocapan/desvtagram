a522d04c1233f467b51fa1d2aa3e575af14dc17f
hoxtocapan <Hoxtocapan@gmail.com>
2023-07-31 23:47:23 -0600

    Deployment comienzo

---
+++ resources/views/perfil/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Editar Perfil: {{ auth()->user()->username }}
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center">
+        <div class="md:w-1/2 bg-white shadow p-6">
+            <form class="mt-10 md:mt-0" method="POST" action="{{ route('perfil.store')}}" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">Username</label>
+
+                    <input type="text" id="username" name="username" placeholder="Tu Nombre de Usuario"
+                        class="border px-3 py-2 w-full rounded-lg @error('username') border-red-500 @enderror"
+                        value="{{ auth()->user()->username }}">
+                    @error('username')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-5">
+                    <label for="imagen" class="mb-2 block uppercase text-gray-500 font-bold">Imagen Perfil</label>
+
+                    <input 
+                        type="file" 
+                        id="imagen" 
+                        name="imagen" 
+                        class="border px-3 py-2 w-full rounded-lg "
+                        accept=".jpg, .jpeg, .png"
+                    >
+                </div>
+
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Email</label>
+                    <input id="email" name="email" type="text" placeholder="Tu Email de Registro"
+                        class="border px-3 py-2 w-full rounded-lg @error('email') border-red-500 @enderror" value="{{ auth()->user()->email }}">
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }}</p>
+                    @enderror
+                </div>
+
+                <input 
+                    type="submit" 
+                    value="Guardar Cambios"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+
+            </form>
+        </div>
+
+    </div>
+@endsection
